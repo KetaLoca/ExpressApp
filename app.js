5b7bba1912d2f68fe2fc72cb02d338591e50589f
KetaLoca <mianramar@gmail.com>
2024-07-22 10:49:57 +0200

    Entendiendo middleware

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ const app = express();
 
 const PORT = process.env.PORT ?? 1234;
 
+app.disable("x-powered-by");
+
+app.use((req, res, next) => {
+  console.log("Aquí iría el middleware"); // middleware que se aplica a todas las rutas
+  // revisar cookies
+  next();
+});
+
+app.use("/pokemon", (req, res, next) => {
+  console.log("Aquí iría el middleware"); // middleware limitado a la ruta pokemon
+  // revisar cookies
+  next(); // es imprescindible utilizar el next al final
+});
+
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Mi página</h1>");
+});
+
+app.use((req, res) => {
+  res.status(201).send("<h1>404 not found</h1>");
 });
 
 app.listen(PORT, () => {
