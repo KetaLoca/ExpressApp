2a335018502c48aa1bba1369bf92058dddf9db02
KetaLoca <mianramar@gmail.com>
2024-07-22 11:36:39 +0200

    middleware migrado a express.json()

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
 const express = require("express");
 const ditto = require("./pokemon/ditto.json");
 const path = require("path");
+const exp = require("constants");
 const app = express();
 
 const PORT = process.env.PORT ?? 1234;
 
 app.disable("x-powered-by");
 
+app.use(express.json());
+
 // middleware que se aplica a todas las rutas
-app.use((req, res, next) => {
-  if (req.method != "POST") return next();
-  if (req.headers["content-type"] != "application/json") return next();
+// app.use((req, res, next) => {
+//   if (req.method != "POST") return next();
+//   if (req.headers["content-type"] != "application/json") return next();
 
-  let body = "";
+//   let body = "";
 
-  req.on("data", (chunk) => {
-    body += chunk.toString();
-  });
+//   req.on("data", (chunk) => {
+//     body += chunk.toString();
+//   });
 
-  req.on("end", () => {
-    const data = JSON.parse(body);
-    data.timestamp = Date.now();
-    //mutar la request para meter la información en el body
-    req.body = data;
-    next();
-  });
-});
+//   req.on("end", () => {
+//     const data = JSON.parse(body);
+//     data.timestamp = Date.now();
+//     //mutar la request para meter la información en el body
+//     req.body = data;
+//     next();
+//   });
+// });
 
 // middleware limitado a la ruta pokemon
 app.use("/pokemon", (req, res, next) => {
