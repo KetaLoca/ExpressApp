52265b0a9e7f3f3bc1f8790956edabeb7e47242c
KetaLoca <mianramar@gmail.com>
2024-07-22 11:30:23 +0200

    Ejemplos de get y post funcionando

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
 const express = require("express");
+const ditto = require("./pokemon/ditto.json");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT ?? 1234;
 
 app.disable("x-powered-by");
 
+// middleware que se aplica a todas las rutas
 app.use((req, res, next) => {
-  console.log("Aquí iría el middleware"); // middleware que se aplica a todas las rutas
-  // revisar cookies
-  next();
+  if (req.method != "POST") return next();
+  if (req.headers["content-type"] != "application/json") return next();
+
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on("end", () => {
+    const data = JSON.parse(body);
+    data.timestamp = Date.now();
+    //mutar la request para meter la información en el body
+    req.body = data;
+    next();
+  });
 });
 
+// middleware limitado a la ruta pokemon
 app.use("/pokemon", (req, res, next) => {
-  console.log("Aquí iría el middleware"); // middleware limitado a la ruta pokemon
+  console.log("Aquí iría el middleware en la ruta /pokemon");
   // revisar cookies
   next(); // es imprescindible utilizar el next al final
 });
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Mi página</h1>");
+});
+
+app.get("/pokemon/ditto", (req, res) => {
+  res.json(ditto);
+});
+
+app.post("/pokemon", (req, res) => {
+  // req.body deberíamos guardar en bbdd
+  res.status(201).json(req.body);
 });
 
 app.use((req, res) => {
